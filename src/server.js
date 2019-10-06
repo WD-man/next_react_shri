66b49fc898fb7bf10e6f89d67f15165c7b1e002d
Deev Pavel <deev@uchi.ru>
2019-10-07 01:34:25 +0300

    readme fix

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ const createApp = require(`./app/app`);
   const server = http.createServer(app);
 
   const PORT = process.env.PORT || 8077;
+  console.log('-------------------------');
+  console.log('Port', PORT);
+  console.log('-------------------------');
 
   server.listen(PORT, () => {
     debug(`server is running on port: ${PORT}`);
